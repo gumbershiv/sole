bf6f0adecbc262486c238340a1cca5e55b19e82d
gumbershiv <98959464+gumbershiv@users.noreply.github.com>
2025-04-28 17:07:10 +0530

    Update b2bBulkProductCopyPaste.js

--- b2bBulkProductCopyPaste.js
+++ b2bBulkProductCopyPaste.js
@@ -5,7 +5,7 @@ import { LightningElement, api, track } from 'lwc';
 import communityId from '@salesforce/community/Id';
 
 // Commerce APIs
-import {refreshCartSummary} from 'commerce/cartApi';
+import { refreshCartSummary } from 'commerce/cartApi';
 
 // Apex code
 import addToCart from '@salesforce/apex/B2BBulkProductToCartController.addToCart';
@@ -14,9 +14,7 @@ import addToCart from '@salesforce/apex/B2BBulkProductToCartController.addToCart
 import { PageLabelStoreMixin } from 'c/b2bPageLabelStoreMixin';
 import { toastUtil, dataGrabber } from 'c/b2bUtil';
 
-export default class B2BCopyPasteOrder extends PageLabelStoreMixin(
-    LightningElement
-) {
+export default class B2BCopyPasteOrder extends PageLabelStoreMixin(LightningElement) {
     constructor() {
         super('b2bCopyPasteOrder', true);
     }
@@ -58,12 +56,12 @@ export default class B2BCopyPasteOrder extends PageLabelStoreMixin(
             },
         ];
     }
+
     async connectedCallback() {
         super.connectedCallback();
     }
 
     validateInputFormat(data) {
-        debugger;
         this.uploadInfo = [];
         this.inputData = [];
         this.errors = [];
@@ -74,16 +72,16 @@ export default class B2BCopyPasteOrder extends PageLabelStoreMixin(
         }
 
         const allRows = data.split(/\r?\n/);
-        const expectedHeader =
-            super.labels.csvHeaderSkuNumber.trim() + ',' + super.labels.csvHeaderQuantity.trim();
+        const expectedHeader = `${super.labels.csvHeaderSkuNumber.trim()},${super.labels.csvHeaderQuantity.trim()}`;
         const start = allRows[0].startsWith(expectedHeader) ? 1 : 0;
 
         for (let i = start; i < allRows.length; i++) {
             const rawRow = allRows[i].trim();
-            if(rawRow === ''){
+            if (rawRow === '') {
                 break;
             }
-            const cleanedRow = rawRow.includes(',') ?rawRow : rawRow.replace('\t', ',');
+
+            const cleanedRow = rawRow.includes(',') ? rawRow : rawRow.replace('\t', ',');
             const columns = cleanedRow.split(',');
 
             const lineNumber = i + 1;
@@ -117,144 +115,88 @@ export default class B2BCopyPasteOrder extends PageLabelStoreMixin(
     }
 
     async addToCart() {
-        let successData = [];
-        let errorData = [];
-
-        const data = this.template.querySelector('[data-id = "fileUpload"]').value;
+        const data = this.template.querySelector('[data-id="fileUpload"]')?.value;
         this.validateInputFormat(data);
 
-        const dataSet = JSON.stringify(this.uploadInfo);
-        this.effectiveAccountId = await dataGrabber.getEffectiveAccountId();
+        if (!this.uploadInfo.length || this.errors.length > 0) {
+            this.isLoading = false;
+            this.showFormatError = true;
+            toastUtil.toastError(this, {
+                title: super.labels.labelError,
+                message: this.errors.length > 0 ? this.errors.join('\n') : super.labels.msgNothingTodoError,
+            });
+            return;
+        }
+
         this.isLoading = true;
+        this.effectiveAccountId = await dataGrabber.getEffectiveAccountId();
 
-        if (this.uploadInfo.length > 0 && this.errors.length === 0) {
-
-            await addToCart({
-                data: dataSet,
+        try {
+            const result = await addToCart({
+                data: JSON.stringify(this.uploadInfo),
                 communityId: communityId,
                 effectiveAccountId: this.effectiveAccountId,
-                isChecked: this.template.querySelector(`[data-id="input-checkbox"]`).checked,
+                isChecked: this.template.querySelector('[data-id="input-checkbox"]')?.checked,
                 cartItemLimit: this.valMaxProductAmount,
-            })
-            
-                .then((result) => {
-                    console.log('result',result);
-                    let showAllSuccessMessage = true;
-                    this.isLoading = false;
-
-                    if (result.isSuccess) {
-                        result.data.forEach((element) => {
-                            let item = {
-                                sku: element.sku,
-                                qty: element.quantity,
-                            };
-
-                            switch (element.message) {
-                                case 'ITEM_SUCCESS':
-                                    element.message = super.labels.msgItemSuccess;
-                                    break;
-                                case 'ERROR_INVALID_SKU':
-                                    element.message = super.labels.msgInvalidSku;
-                                    break;
-                                case 'ERROR_INVALID_QUANTITY':
-                                    element.message = super.labels.msgInvalidQty;
-                                    break;
-                                case 'ERROR_DUPLICATE_SKU':
-                                    element.message = super.labels.msgDuplicateSku;
-                                    break;
-                                case 'ERROR_QUANTITY_RULES':
-                                    element.message = 'Invalid Quanitity';
-                                    element.message = String.format(
-                                        super.labels.msgInvalidQtyRules,
-                                        [element.minimum, element.maximum, element.increment]
-                                    );
-                                    break;
-                                case 'ITEM_REMOVED':
-                                    element.message = super.labels.msgItemRemoved;
-                                    break;
-                                default:
-                                    element.message = element.message;
-                            }
-
-                            item.notes = element.message;
-                            item.notesColor = element.isSuccess
-                                ? 'slds-text-color_success'
-                                : 'slds-text-color_error';
-
-                            if (element.isSuccess) {
-                                successData.push(item);
-                            } else {
-                                errorData.push(item);
-                                showAllSuccessMessage = false;
-                            }
-                        });
-
-                        this.tableData = this.inputData.concat(errorData, successData);
-
-                        if (this.inputData.length > 0) {
-                            showAllSuccessMessage = false;
-                        }
-
-                        if (showAllSuccessMessage) {
-                            toastUtil.toastSuccess(this, {
-                                title: super.labels.labelSuccess,
-                                message: super.labels.msgAddToCartAllSuccess,
-                            });
-                        } else {
-                            toastUtil.toastInfo(this, {
-                                title: super.labels.labelPartialSuccess,
-                                message: super.labels.msgAddToCartNotAllSuccess,
-                            });
-                        }
-
-                        this.showResultTable = true;
-                        this.showInputForm = false;
-
-                        refreshCartSummary()
-                        .then(() => {
-                            console.log('Success');
-                        });
-                        
-                    } else {
-                        this.showResultTable = false;
-                        this.showInputForm = true;
-                        if (result.message = 'ERROR_UPLOAD_LIMIT') {
-                            result.message = super.labels.msgUploadLimit;
-                        }
-                        toastUtil.toastError(this, {
-                            title: super.labels.labelError,
-                            message: result.message,
-                        });
-                    }
-                })
-                .catch((error) => {
-                    this.isLoading = false;
-                    let message = error && error.body && error.body.message;
-                    toastUtil.toastError(this, {
-                        title: super.labels.labelError,
-                        message: message,
-                    });
-                });
-        } else if (this.errors.length > 0) {
-            showFormatError = true;
-            this.isLoading = false;
-            const displayError = this.errors.join('\n')
-            toastUtil.toastError(this, {
-                title: super.labels.labelError,
-                message: this.errors.join('\n'),
             });
-        } else {
+
+            this.isLoading = false;
+
+            if (result.isSuccess) {
+                this.processResultData(result.data);
+            } else {
+                this.handleAddToCartError(result.message);
+            }
+        } catch (error) {
             this.isLoading = false;
             toastUtil.toastError(this, {
                 title: super.labels.labelError,
-                message: super.labels.msgNothingTodoError,
+                message: error?.body?.message || super.labels.labelUnknownError,
             });
         }
     }
 
-    startOver() {
-        this.showResultTable = false;
-        this.showInputForm = true;
-        
-    }
-}
+    processResultData(resultData) {
+        let successData = [];
+        let errorData = [];
+        let showAllSuccessMessage = true;
+
+        resultData.forEach((element) => {
+            let item = {
+                sku: element.sku,
+                qty: element.quantity,
+                notes: this.getMessageForElement(element),
+                notesColor: element.isSuccess ? 'slds-text-color_success' : 'slds-text-color_error',
+            };
+
+            if (element.isSuccess) {
+                successData.push(item);
+            } else {
+                errorData.push(item);
+                showAllSuccessMessage = false;
+            }
+        });
+
+        this.tableData = this.inputData.concat(errorData, successData);
+
+        if (this.inputData.length > 0) {
+            showAllSuccessMessage = false;
+        }
+
+        if (showAllSuccessMessage) {
+            toastUtil.toastSuccess(this, {
+                title: super.labels.labelSuccess,
+                message: super.labels.msgAddToCartAllSuccess,
+            });
+        } else {
+            toastUtil.toastInfo(this, {
+                title: super.labels.labelPartialSuccess,
+                message: super.labels.msgAddToCartNotAllSuccess,
+            });
+        }
+
+        this.showResultTable = true;
+        this.showInputForm = false;
+
+        refreshCartSummary()
+            .then(() => console.log('
